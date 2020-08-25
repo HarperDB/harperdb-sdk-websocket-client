92e93c099693dc62ecf97c791f3c8d2cf0e1fd63
Ethan Arrowood <ethan.arrowood@gmail.com>
2020-08-24 23:43:45 -0400

    working on insert test

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 'use strict'
 
+const got = require('got')
+
 const tap = require('tap')
+
 const HarperDBWebSocketClient = require('./index.js')
 
 const socketClusterOptions = {
@@ -75,4 +78,85 @@ tap.test('login handler fires', t => {
 		}
 	})
 	client.init()
+})
+
+tap.skip('can insert records', t => {
+	// reset db using http
+	const dropTable = () => got.post('http://localhost', {
+		port: 9925,
+		headers: {
+			Authorization: 'Basic SERCX0FETUlOOnBhc3N3b3Jk'
+		},
+		retry: 0,
+		json: {
+			'operation': 'drop_table',
+			'schema': 'dev',
+			'table': 'dog'
+		}
+	})
+
+	const createTable = () => got.post('http://localhost', {
+		port: 9925,
+		headers: {
+			Authorization: 'Basic SERCX0FETUlOOnBhc3N3b3Jk'
+		},
+		retry: 0,
+		json: {
+			'operation': 'create_table',
+			'schema': 'dev',
+			'table': 'dog',
+			'hash_attribute': 'id'
+		}
+	})
+
+	dropTable()
+		.then(() => {
+			createTable()
+				.catch(err => {
+					console.error(err)
+					t.fail()
+					t.end()
+				})
+		})
+		.catch(err => {
+			const { response } = err
+			if (response && response.statusCode === 500 && JSON.parse(response.body)['error'] === 'Table \'dog\' does not exist in schema \'dev\'') {
+				createTable()
+					.catch(() => {
+						t.fail()
+						t.end()
+					})
+			} else {
+				t.fail()
+				t.end()
+			}
+		})
+
+	client = new HarperDBWebSocketClient({
+		hostname: 'localhost',
+		socketClusterOptions,
+		port: 1111,
+		username: 'cluster_user',
+		password: 'password',
+		implicitInit: true
+	})
+
+	const schema = 'dev'
+	const table = 'dog'
+	const records = [
+		{ id: '1', name: 'Harper', breed: 'Mutt' },
+		{ id: '2', name: 'Penny', breed: 'Mutt' }
+	]
+	client.subscribe(`${schema}:${table}`, data => {
+		console.log(data)
+		t.equal(data.type, 'HDB_TRANSACTION')
+		t.equal(data.transaction.operation, 'insert')
+		t.equal(data.transaction.schema, schema)
+		t.equal(data.transaction.table, table)
+		t.same(data.transaction.records, records)
+		t.equal(data.schema, schema)
+		t.equal(data.table, table)
+		t.end()
+	})
+	client.insert(`${schema}:${table}`, records)
 })

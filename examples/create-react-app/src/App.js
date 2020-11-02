5a1c4d3f7bbd8b0a41a41f5f71fa5c6e2f65de2e
deliciousmonster <jaxon@deliciousmonster.com>
2020-11-02 11:44:17 -0700

    Update App.js

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from 'react';
 import './App.css';
 import HarperDBWebSocketClient from 'harperdb-websocket-client';
 
+// initialize the client outside the render cycle
 const client = new HarperDBWebSocketClient({
   hostname: 'localhost',
   socketClusterOptions: {
